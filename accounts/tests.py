ec29c00592466b3fe5f381146e92f9394da6394a
bugulin <petriczech12@gmail.com>
2017-07-18 15:18:27 +0200

    Přidány testy do aplikací 'accounts' a 'core'.

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Account
+
+
+class AccountTestCase(TestCase):
+    def setUp(self):
+        Account.objects.create_user('user1', 'user1@gymgeek.cz', 'U1')
+        Account.objects.create_user('user2', 'user2@gymgeek.cz', 'U2')
+
+        self.client = Client()
+        self.client.login(username='user1', password='U1')
+
+    def test_accessibility_of_account_pages(self):
+        """Uživatel může upravovat jen svoje informace."""
+        # Úprava vlastního účtu
+        response = self.client.get(reverse('accounts:edit', args=('user1',)))
+        self.assertEqual(response.status_code, 200)
+
+        # Úprava cizího účtu
+        response = self.client.get(reverse('accounts:edit', args=('user2',)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_account_form_processing(self):
+        """Správné zpracování aktualizace informací účtu."""
+        url = reverse('accounts:edit', args=('user1',))
+
+        # Správná aktualizace
+        response = self.client.post(url, {'about': 'Bez popisu.'})
+        self.assertEqual(response.status_code, 302)
+
+        # Nic nezměněno
+        response = self.client.post(url, {'about': 'Bez popisu.'})
+        self.assertEqual(response.status_code, 200)
+
+        # Nesprávná hodnota (about: max_length=500)
+        response = self.client.post(url, {'about': '_'*501})
+        self.assertEqual(response.status_code, 200)
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from accounts.models import Account
+
+
+class CoreTestCase(TestCase):
+    def setUp(self):
+        Account.objects.create_user('user', 'user@gymgeek.cz', 'password')
+        self.client = Client()
+
+    def test_home_page(self):
+        """Přihlášený uživatel vidí na adrese '/' domovskou stránku (home.html), zatímco nepřihlášený obrazovku s přihlašovacím odkazem (login.html)."""
+        # Anonymní uživatel
+        response = self.client.get('/')
+        self.assertEqual(response.templates[0].name, 'core/login.html')
+        self.assertIn('href=\"{}\"'.format(reverse('social:begin', args=('google-oauth2',))), str(response.content))
+
+        # Registrovaný uživatel
+        self.client.login(username='user', password='password')
+        response = self.client.get('/')
+        self.assertEqual(response.templates[0].name, 'core/home.html')
+
+    def test_health_page(self):
+        """Na adrese '/health' je veřejně přístupná stránka."""
+        # Anonymní uživatel
+        response = self.client.get('/health')
+        self.assertEqual(response.status_code, 200)
+
+        # Registrovaný uživatel
+        self.client.login(username='user', password='password')
+        response = self.client.get('/health')
+        self.assertEqual(response.status_code, 200)
