a168c2d0139d5767ad83a8df1ce70beaed0232ba
bugulin <petriczech12@gmail.com>
2017-09-12 19:34:29 +0200

    Vlastní model pro skupinu (Group). Přizpůsobení administrátorských stránek.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.utils.translation import ugettext, ugettext_lazy as _
-from .models import Account
+from django.utils.translation import ugettext_lazy as _
 
+from django.contrib.auth.models import Group as DjangoGroup
+from .models import Account, Group
+
+admin.site.unregister(DjangoGroup)
+
+
+@admin.register(Account)
 class AccountAdmin(UserAdmin):
+    list_display = ('get_full_name', 'email', 'is_superuser', 'is_active')
+    search_fields = ('first_name', 'last_name', 'username')
+    ordering = ('is_active', 'last_name', 'first_name')
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'url', 'avatar', 'about')}),
@@ -12,4 +21,31 @@ class AccountAdmin(UserAdmin):
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
         )
 
-admin.site.register(Account, AccountAdmin)
+    def get_full_name(self, account):
+        return account.get_full_name()
+
+    get_full_name.short_description = _('full name')
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'account_count')
+    search_fields = ('name',)
+    ordering = ('name',)
+    filter_horizontal = ('permissions',)
+    fieldsets = (
+        (None, {'fields': ('name', 'code')}),
+        (_('Permissions'), {'fields': ('permissions',)})
+    )
+
+    def formfield_for_manytomany(self, db_field, request=None, **kwargs):
+        if db_field.name == 'permissions':
+            qs = kwargs.get('queryset', db_field.remote_field.model.objects)
+            kwargs['queryset'] = qs.select_related('content_type')
+
+        return super(GroupAdmin, self).formfield_for_manytomany(db_field, request=request, **kwargs)
+
+    def account_count(self, group):
+        return len(group.account_set.all())
+
+    account_count.short_description = _('account count')
--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group as DjangoGroup
 from django.utils.translation import ugettext_lazy as _
+
+class Group(DjangoGroup):
+    code = models.CharField(_('secret code'), max_length=32)
+
+    class Meta:
+        verbose_name = _('group')
+        verbose_name_plural = _('groups')
+
 
 class Account(AbstractUser):
     first_name = models.CharField(_('first name'), max_length=30)
@@ -10,3 +18,5 @@ class Account(AbstractUser):
     avatar = models.CharField(_('profile image'), max_length=250)
     url = models.CharField(_('google+ page'), max_length=75)
     about = models.TextField(_('description'), max_length=500, blank=True)
+
+    groups = models.ManyToManyField(Group, verbose_name=_('groups'), blank=True, related_name="account_set", related_query_name="account")
