51480b9dd69c177daecb8a23936535e7c66d722f
bugulin <petriczech12@gmail.com>
2017-09-18 19:00:52 +0200

    Hostování na Heroku.

--- gymgeek/database.py
+++ gymgeek/database.py
@@ -1,17 +1,11 @@
 import os
+import dj_database_url
 from django.conf import settings
 
 def config():
     # Production
     if os.getenv('DATABASE_SERVICE_NAME') == 'postgresql':
-        default = {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.getenv('DATABASE_NAME'),
-            'USER': os.getenv('DATABASE_USER'),
-            'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-            'HOST': os.getenv('POSTGRESQL_SERVICE_HOST'),
-            'PORT': os.getenv('POSTGRESQL_SERVICE_PORT'),
-        }
+        default = dj_database_url.config()
 
     # Development
     else:
