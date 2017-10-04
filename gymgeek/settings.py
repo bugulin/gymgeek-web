57117e996b7b4cd0a8752bb14c9f6f9eb494ab99
bugulin <petriczech12@gmail.com>
2017-10-04 18:05:06 +0200

    Opraven problém se získáváním dat z Google.

--- gymgeek/settings.py
+++ gymgeek/settings.py
@@ -125,8 +125,9 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.user.create_user',
     'social_core.pipeline.social_auth.associate_user',
     'accounts.pipeline.authorize',
-    'social_core.pipeline.social_auth.load_extra_data',
     'accounts.pipeline.load_extra_data',
+    'social_core.pipeline.social_auth.load_extra_data',
+
     'social_core.pipeline.user.user_details',
 )
 
