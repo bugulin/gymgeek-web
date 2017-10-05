96c4bb10cff2f26eab7f29a2b3b1430529935a49
bugulin <petriczech12@gmail.com>
2017-10-05 16:53:42 +0200

    Nastylování příspěvků.

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 
 class Post(models.Model):
     title = models.CharField(_('title'), max_length=50)
-    text = models.TextField(_('text'), max_length=1000)
+    text = models.TextField(_('text'), max_length=2000)
 
     is_visible = models.BooleanField(_('visible'), default=True)
     is_pinned = models.BooleanField(_('pinned'), default=False)
